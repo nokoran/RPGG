6de2c2bd1ad57f93796c196d3250a2beb89ce1ab
Fangornibro <kuzmindima2002@gmail.com>
2021-12-23 23:15:23 +0200

    Chistka2

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,23 +5,4 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public GameObject DoorTop, DoorRight, DoorBottom, DoorLeft;
-    public Transform[] floor;
-
-    public void RotateRandomly()
-    {
-        int Count = Random.Range(0, 4);
-        for(int i = 0; i < Count; i++)
-        {
-            transform.Rotate(0, 0, -90);
-            for (int e = 0; e < floor.Length; e++)
-            {
-                floor[e].Rotate(new Vector3Int(0, 0, 90));
-            }
-            GameObject tmp = DoorLeft;
-            DoorLeft = DoorBottom;
-            DoorBottom = DoorRight;
-            DoorRight = DoorTop;
-            DoorTop = tmp;
-        }
-    }
 }
