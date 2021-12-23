f1ac0c2cef597a22305ad63226949241823ecd46
nokoran <nokoran.png@gmail.com>
2021-12-23 13:55:59 +0200

    Items and Walls Update 2.0

--- Assets/Scripts/CanvasScript.cs
+++ Assets/Scripts/CanvasScript.cs
@@ -8,29 +8,36 @@ public class CanvasScript : MonoBehaviour
 {
     private Image[] MyItems;
     public Text tears, speed, range, shotspeed;
+    public static Text tears1, speed1, range1, shotspeed1;
     private static Image m_Image;
     public Canvas Canvas;
-    public static Canvas myCanvas;
+    public static Canvas MyCanvas;
 
     private void Start()
     {
-        myCanvas = Canvas;
-        tears.text += Player.tears;
-        speed.text += Player.speed;
-        range.text += Bullet.range;
-        shotspeed.text += Bullet.shotspeed;
-
-    }
-    private void Update()
-    {
+        MyCanvas = Canvas;
+        tears1 = tears;
+        speed1 = speed;
+        range1 = range;
+        shotspeed1 = shotspeed;
         
+        StatsChanged();
     }
+
+    public static void StatsChanged()
+    {
+        tears1.text = "Tears: " + Convert.ToString(Player.tears);
+        speed1.text = "Speed: " + Convert.ToString(Player.speed);
+        range1.text = "Range: " + Convert.ToString(Bullet.range);
+        shotspeed1.text = "ShotSpeed: " + Convert.ToString(Bullet.shotspeed);
+    }
+    
     public static void AddNewItem(Sprite ItemImage)
     {
         M1:
         var newItem = (GameObject) Instantiate(Resources.Load("ItemImageTemplate", typeof(GameObject))) as GameObject;
         if (newItem == null) goto M1;
-        newItem.gameObject.transform.SetParent(myCanvas.transform);
+        newItem.gameObject.transform.SetParent(MyCanvas.transform);
         newItem.gameObject.GetComponent<Image>().sprite = ItemImage;
         newItem.transform.localScale = Vector3.one;
         newItem.transform.localRotation = Quaternion.Euler (Vector3.zero);
